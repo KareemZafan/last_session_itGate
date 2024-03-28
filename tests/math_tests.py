8293ea91d966ab44ffe4dbfa4259e6dae5e0b80d
KareemZafan <kareemm.zafan@gmail.com>
2024-03-29 00:14:06 +0200

    failed tests

--- tests/math_tests.py
+++ tests/math_tests.py
@@ -1,13 +1,9 @@
 
 
 def test_add():
-    assert (2+3) <= 5
+    assert (2+3) < 5
 
 
 def test_capitalize():
     "hello".capitalize() == "Hello"
     
-
-
-def test_capitalize():
-    "hello".upper() == "Hello"
